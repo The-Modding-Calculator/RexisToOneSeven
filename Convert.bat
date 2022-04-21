17b2006744b8c3b6451a8b902d564ef4cc38e676
The-Modding-Calculator <103147643+The-Modding-Calculator@users.noreply.github.com>
2022-04-21 09:55:12 +0100

    Added Find_files_missing_in_OneSeven_dump to the convert.bat

--- Convert.bat
+++ Convert.bat
@@ -1,3 +1,4 @@
 python Create_multi_hash_dumped_tex.py "OneSeven Raw Texture Dumps" "Multi Hash Raw Texture Dumps"
 python Rename_Rexis_hashes_to_OneSeven_hashes.py "Multi Hash Raw Texture Dumps" "Rexis Replacement Textures" "OneSeven Replacement Textures" --doubleCheck
+python Find_files_missing_in_OneSeven_dump.py "Multi Hash Raw Texture Dumps" "Rexis Replacement Textures" "Need to dump"
 python texture_dump_alpha_scaler.py unscale "OneSeven Replacement Textures" --force
